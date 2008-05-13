7f59639ac6960cbd0dba1a6f47879d4c4735b689
jeremybubs <jeremybubs@fb5b4802-194b-0410-9e11-5134f6b347ec>
2008-05-13 16:46:40 +0000

    not much
    git-svn-id: https://pascalinterpreterinjava.googlecode.com/svn/trunk@13 fb5b4802-194b-0410-9e11-5134f6b347ec

--- pascalTypes/custom_type.java
+++ pascalTypes/custom_type.java
@@ -1,11 +1,15 @@
 package pascalTypes;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.TreeMap;
 import java.util.Map.Entry;
 
+import preprocessed.variable_declaration;
+
 public class custom_type implements pascal_type {
-	public custom_type(TreeMap<String, Class> types) {
+	public custom_type(LinkedList<variable_declaration> types) {
+		
 		for (Entry<String, Class> e : types.entrySet()) {
 			value.put(e.getKey(), new standard_var(e.getValue()));
 		}
