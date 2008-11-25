88f3e5201e2929a0a4cdca73e2c71657aebf86aa
jeremybubs <jeremybubs@fb5b4802-194b-0410-9e11-5134f6b347ec>
2008-11-25 03:25:29 +0000

    git-svn-id: https://pascalinterpreterinjava.googlecode.com/svn/trunk@53 fb5b4802-194b-0410-9e11-5134f6b347ec

--- plugins/ChangeIt.java
+++
@@ -1,9 +0,0 @@
-package plugins;
-
-import processing.pascal_plugin;
-
-public class ChangeIt implements pascal_plugin {
-	public static void changeIt(String text) {
-		text = "OMG!"; // TODO
-	}
-}
