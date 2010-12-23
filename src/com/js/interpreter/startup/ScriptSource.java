2829cdefafcbae39b2345f534e048fa3f79820cd
jeremybubs <jeremybubs@fb5b4802-194b-0410-9e11-5134f6b347ec>
2010-12-23 03:10:42 +0000

    Documented scriptsource
    git-svn-id: https://pascalinterpreterinjava.googlecode.com/svn/trunk@155 fb5b4802-194b-0410-9e11-5134f6b347ec

--- src/com/js/interpreter/startup/ScriptSource.java
+++ src/com/js/interpreter/startup/ScriptSource.java
@@ -3,6 +3,19 @@ package com.js.interpreter.startup;
 import java.io.Reader;
 
 public interface ScriptSource {
-public String[] list();
-public Reader read(String scriptname);
+	/**
+	 * List possible script contents by name.
+	 * 
+	 * @return list of source names
+	 */
+	public String[] list();
+
+	/**
+	 * Open a stream to a given source content.
+	 * 
+	 * @param scriptname
+	 *            The name of the source
+	 * @return A reader attached to that source
+	 */
+	public Reader read(String scriptname);
 }
