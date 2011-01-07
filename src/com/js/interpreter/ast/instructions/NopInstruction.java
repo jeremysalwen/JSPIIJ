7a40d8a2c53620ed22159aeaa3f65982981fcab5
jeremybubs <jeremybubs@fb5b4802-194b-0410-9e11-5134f6b347ec>
2011-01-07 04:25:34 +0000

    Added Nop Instruction.
    git-svn-id: https://pascalinterpreterinjava.googlecode.com/svn/trunk@188 fb5b4802-194b-0410-9e11-5134f6b347ec

---
+++ src/com/js/interpreter/ast/instructions/NopInstruction.java
@@ -0,0 +1,26 @@
+package com.js.interpreter.ast.instructions;
+
+import com.js.interpreter.linenumber.LineInfo;
+import com.js.interpreter.runtime.VariableContext;
+import com.js.interpreter.runtime.codeunit.RuntimeExecutable;
+import com.js.interpreter.runtime.exception.RuntimePascalException;
+
+public class NopInstruction extends DebuggableExecutable {
+	LineInfo line;
+
+	public NopInstruction(LineInfo line) {
+		this.line = line;
+	}
+
+	@Override
+	public ExecutionResult executeImpl(VariableContext f,
+			RuntimeExecutable<?> main) throws RuntimePascalException {
+		return ExecutionResult.NONE;
+	}
+
+	@Override
+	public LineInfo getLineNumber() {
+		return line;
+	}
+
+}
