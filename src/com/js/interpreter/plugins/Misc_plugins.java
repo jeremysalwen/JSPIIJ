331cae1ac22037d69beac2d3177f951d09baae2a
jeremybubs <jeremybubs@fb5b4802-194b-0410-9e11-5134f6b347ec>
2011-01-12 23:38:14 +0000

    Added length function for strings.
    git-svn-id: https://pascalinterpreterinjava.googlecode.com/svn/trunk@208 fb5b4802-194b-0410-9e11-5134f6b347ec

--- src/com/js/interpreter/plugins/Misc_plugins.java
+++ src/com/js/interpreter/plugins/Misc_plugins.java
@@ -26,9 +26,19 @@ public class Misc_plugins implements PascalPlugin {
 		a.set(result);
 	}
 
+	@MethodTypeData(info = { @ArrayBoundsInfo(starts = { 0 }, lengths = { 0 }),
+			@ArrayBoundsInfo })
+	public static void setLength(VariableBoxer<Object[]> a, int length) throws RuntimePascalException {
+		SetArrayLength(a,length);
+	}
+	
 	@MethodTypeData(info = { @ArrayBoundsInfo(starts = { 0 }, lengths = { 0 }) })
 	public static int length(Object[] o) {
 		return o.length;
+	}
+
+	public static int length(StringBuilder s) {
+		return s.length();
 	}
 
 	@Override
