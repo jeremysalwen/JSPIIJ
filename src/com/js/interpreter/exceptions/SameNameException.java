566d9712a79baeeceac5ac9bc124bb7d39f514c8
jeremybubs <jeremybubs@fb5b4802-194b-0410-9e11-5134f6b347ec>
2011-01-17 08:13:22 +0000

    Updated message to be nicer.
    git-svn-id: https://pascalinterpreterinjava.googlecode.com/svn/trunk@221 fb5b4802-194b-0410-9e11-5134f6b347ec

--- src/com/js/interpreter/exceptions/SameNameException.java
+++ src/com/js/interpreter/exceptions/SameNameException.java
@@ -6,8 +6,7 @@ import com.js.interpreter.linenumber.LineInfo;
 public class SameNameException extends ParsingException {
 	public SameNameException(LineInfo line, NamedEntity previous,
 			NamedEntity n, String name) {
-		super(line, n.getEntityType() + " " + name + " defined at "
-				+ n.getLineNumber() + " conflicts with previously defined "
+		super(line, n.getEntityType() + " " + name + " conflicts with previously defined "
 				+ previous.getEntityType() + " with the same name defined at "
 				+ previous.getLineNumber());
 	}
