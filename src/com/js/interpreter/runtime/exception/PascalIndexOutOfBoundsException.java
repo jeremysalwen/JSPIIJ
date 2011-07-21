71ecbd0e9f0a0358038cd9975cc1f556af6f8fb7
jeremybubs <jeremybubs@fb5b4802-194b-0410-9e11-5134f6b347ec>
2011-07-21 08:21:38 +0000

    Give better error message for uninitialized VLAs
    git-svn-id: https://pascalinterpreterinjava.googlecode.com/svn/trunk@277 fb5b4802-194b-0410-9e11-5134f6b347ec

--- src/com/js/interpreter/runtime/exception/PascalIndexOutOfBoundsException.java
+++ src/com/js/interpreter/runtime/exception/PascalIndexOutOfBoundsException.java
@@ -19,6 +19,8 @@ public class PascalIndexOutOfBoundsException extends RuntimePascalException {
 	private String getcause(int index, int min, int max) {
 		if (index < min) {
 			return index + " is less than the minimum index of " + min;
+		} else if (max == -1 && min == 0) {
+			return "variable length array has not been initialized yet";
 		} else {
 			return index + " is greater than the maximum index of " + max;
 		}
